ba31bd5ebae6949ec6b944bac3af09b26809fec9
Luca Errani <luca.errani@fiscozen.it>
2025-09-26 19:15:05 +0200

    chore: setup shadcn ui

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-
+import { Button } from '@/components/ui/button';
 </script>
 
 <template>
   <h1 class="bg-blue-500">Test page</h1>
+  <Button class="m-4">Click me</Button>
 </template>
